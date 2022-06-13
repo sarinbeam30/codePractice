fdbad406972a366d59a01732abdea0ce1f5fb55e
Sarin Wanichwasin <sarin_beam30@hotmail.com>
2022-06-13 09:50:39 +0700

    create navObject in Section_2_Question_3

--- PRETEST_FRONTEND_TASK/Section_2_Question_3.js
+++ PRETEST_FRONTEND_TASK/Section_2_Question_3.js
@@ -11,8 +11,13 @@ let supportDomParser = () => {
   return true;
 };
 
+let searchIndex = (string, searchTerm) => {
+  return string.indexOf(searchTerm)
+};
+
 let getNavData = async () => {
   try {
+    let navObject = {}
     let fetchData = await fetch("https://codequiz.azurewebsites.net/", {
       method: "GET",
       headers: {
@@ -25,7 +30,24 @@ let getNavData = async () => {
     if (support) {
       let parser = new DomParser();
       let doc = parser.parseFromString(jsonData, "text/html").rawHTML;
-      console.log(doc);
+      let startIndex = searchIndex(doc, '<table>')
+      let lastIndex = searchIndex(doc, '<input')
+      doc = doc.slice(startIndex, lastIndex)
+      doc = doc.split('<tr>')
+      
+      for (let i=2; i<doc.length; i++) {
+        doc[i] = doc[i].split('<td>')
+        for (let j=1; j<doc[i].length; j++) {
+          if (j === 1 ) {
+            if (doc[i][1] === 'BM70SSF </td>') {
+              navObject[doc[i][1].slice(0, -6).toString() ] = doc[i][2].slice(0, -5).toString()
+            } else {
+              navObject[doc[i][1].slice(0, -5).toString()] = doc[i][2].slice(0, -5).toString()
+            }
+          }
+        }
+      }
+      console.log(navObject)
     } else {
       console.log("Not Support DomParser");
     }
